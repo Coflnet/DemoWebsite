755f0afac58a21ae2bf09e160daf8305b0ddda20
Florian Muehlhans <muehlhans.f@coflnet.com>
2024-09-02 17:41:28 +0200

    use fetch to download the xml file

--- components/generate-surveys.vue
+++ components/generate-surveys.vue
@@ -8,6 +8,7 @@ const isLoading = ref(false);
 const surverys = ref(null);
 const headers = ref(null);
 const count = ref(100);
+const asExcel = ref(false);
 
 const uploadFile = async () => {
   if (file.value) {
@@ -16,11 +17,53 @@ const uploadFile = async () => {
 
     try {
       isLoading.value = true;
-      const response = await axios.post(`https://demo.coflnet.com/api/import/generate?count=${count.value}`, formData, {
+
+      let url = `https://demo.coflnet.com/api/import/generate?count=${count.value}`;
+      if (asExcel.value) {
+        let filename = `whatever.xlsx`;
+        const formData = new FormData()
+        formData.append('file', file.value)
+
+        let res = await fetch(`http://localhost:5159/api/import/generateExcel/${count.value}`, {
+          method: 'POST',
+          body: formData,
+        });
+        let blob = await res.blob();
+
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+
+        return;
+      }
+
+      const response = await axios.post(url, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       })
+
+      if (asExcel.value) {
+        // response.data is the raw bytes
+
+        const blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'survey.xlsx';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+
+        return;
+      }
+
       console.log('File uploaded successfully:', response.data)
 
       let el = response.data[0];
@@ -70,6 +113,9 @@ const uploadFile = async () => {
                 v-model="count"
             >
             </v-number-input>
+          </v-col>
+          <v-col>
+            <v-checkbox label="As Excel" v-model="asExcel"></v-checkbox>
           </v-col>
           <v-col>
             <v-btn class="mt-2" block @click="uploadFile">Submit</v-btn>
